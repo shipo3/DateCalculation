40d5f83e465f1d13b5ed82af347043b3605a8d6d
shipo3 <yfb13555@nifty.com>
2022-06-22 03:38:58 +0900

    mapperテスト

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,12 @@
+DROP ALL OBJECTS;
+ 
+ CREATE TABLE IF NOT EXISTS formula
+ (
+ id int  AUTO_INCREMENT,
+ name varchar(30) NOT NULL,
+ detail varchar(100),
+ year int NOT NULL,
+ month int NOT NULL,
+ day int NOT NULL
+ );
+ 
